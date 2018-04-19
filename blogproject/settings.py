975c1f0f2815a685d7187be7e6685f49949f979e
Guo Zhen <2997871698@qq.com>
2018-04-19 21:07:33 +0800

    modify allowhost

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'rz5maby$_j7k@ab3gu8!88&hff0!=m!a2397nd-t@cu*s5owpj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.guozhen.kim']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.guozhen.kim','39.105.32.32']
 
 
 # Application definition
