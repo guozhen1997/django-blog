37a25fe3105c4e53c19501338d7f7bbbf4019f61
Guo Zhen <2997871698@qq.com>
2018-04-20 16:05:43 +0800

    add app polls

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,29 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 from comments.models import Comment
+from polls.models import Question,Choice
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
-# 把新增的 PostAdmin 也注册进来
+# 把新增的polls也注册进来
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+
+admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('blog.urls')),
+    url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
+    url(r'^polls', include('polls.urls')),
 ]
